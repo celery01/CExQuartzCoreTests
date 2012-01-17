b56a738c1c7700d977711c8dba1d47fbd5cb19d4
celery01 <celery01@gmail.com>
2012-01-17 18:27:38 +0800

    import

---
+++ CExQuartzCoreTests/CExQuartzCoreOrganizerController.h
@@ -0,0 +1,13 @@
+//
+//  CExQuartzCoreOrganizerController.h
+//  DrawTransformer
+//
+//  Created by Daniel Lin on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CExQuartzCoreOrganizerController : UITableViewController
+
+@end
